b75512c5b99d7f9ccf484485fbb7140b6bf39be9
aryandas777 <73943437+aryandas777@users.noreply.github.com>
2021-02-10 16:23:42 +0530

    Add files via upload

--- AC.js
+++ AC.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 
 function preload(){
   img = loadImage('AC.jpg');
@@ -24,15 +25,26 @@ function gotResult(error, results) {
     console.log(error);
   }
   console.log(results);
+  objects = results;
 }
 
 
 function draw() {
   image(img, 0, 0, 640, 420);
-  fill("#FF0000");
-  text("AC", 45, 75);
-  noFill();
-  stroke("#FF0000");
-  rect(30, 60, 500, 350 );
+  
+   if(status != "")
+   {
+     for(i = 0; i < objects.length; i++)
+     {
+      document.getElementById("status").innerHTML = "Status : Object Detected";
+
+      fill('#FF0000');
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke('#FF0000');
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+     }
+   }
 
 }
